7105debba3ae28359d9aaabcf1379f9d40f51ee8
ruphia022799@gmail.com <ruphia022799@gmail.com>
2022-10-25 13:48:18 +0800

    Calculator

--- script.js
+++ script.js
@@ -1,44 +1,34 @@
-const acEl = document.querySelector('.ac');
-const pmEl = document.querySelector('.pm');
-const prcntEl = document.querySelector('.percent');
-const specialEl = document.querySelectorAll('.special');
-
-const divideEl = document.querySelector('.divide');
-const timeEl = document.querySelector('.times');
-const minusEl = document.querySelector('.minus');
-const plusEl = document.querySelector('.plus');
-const equalEl = document.querySelector('.equal');
-
+//DOM Elements
 const operatorEl = document.querySelectorAll('.operator');
 
 
-const periodEl = document.querySelector('.period');
-const oneEl = document.querySelector('.one');
-const twoEl = document.querySelector('.two');
-const threeEl = document.querySelector('.three');
-const fourEl = document.querySelector('.four');
-const fiveEl = document.querySelector('.five');
-const sixEl = document.querySelector('.six');
-const sevenEl = document.querySelector('.seven');
-const eightEl = document.querySelector('.eight');
-const nineEl = document.querySelector('.nine');
-const zeroEl = document.querySelector('.zero')
 const numberEl = document.querySelectorAll('.number');
 const output = document.querySelector('.output');
 
+//Functions
+function pressNum(event) {
+  if (output.textContent === '0') {
+    output.textContent = event.target.textContent;
+  } else {
+    output.textContent += event.target.textContent;
+    console.log(event.target.textContent);
+  }
+}
 
-
+function pressOperator(event) {
+  let lastOperator = output.textContent.slice(-1);
+  if (lastOperator === plusEl || lastOperator === minusEl || lastOperator === timesEl || lastOperator === divideEl) {
+   output.textContent = output.textContent.slice(0, -1) + event.target.textContent;
+  } else {
+    output.textContent += event.target.textContent;
+    console.log(target.value);
+  }
+}
 numberEl.forEach(btn => {
-  btn.addEventListener('click', event => {
-    output.textContent = event.target.textContent;
-    console.log(btn);
-  });
+  btn.addEventListener('click', pressNum)
 });
 
-operatorEl.forEach(btn => {
-  btn.addEventListener('click', event => {
-    output.textContent = event.target.textContent;
-    console.log(btn);
+ operatorEl.forEach(btn => {
+ btn.addEventListener('click', pressOperator)
   });
-});
 
